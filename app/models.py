a82cbf557dc73b7da7314dbef2cc1b56a92ee947
Scroboticc <scrobotic1@gmail.com>
2026-01-09 11:08:42 +0100

    fixed spacing error

--- app/models.py
+++ app/models.py
@@ -11,9 +11,10 @@ class Account(db.Model):
     last_name = db.Column(db.String(50), nullable=True)
     balance = db.Column(db.Numeric(11, 2), nullable=False, default=0.00)
     pin_hash = db.Column(db.String(255), nullable=True)
-	username = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False)
     def set_pin(self, pin):
         self.pin_hash = hash_pin(pin)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}".strip()
+        #hallo
