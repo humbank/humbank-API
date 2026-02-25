499d74e213fc1dc2b8ee677b2102a55de0b4cf50
Humbank <humbank.ulm@gmail.com>
2026-02-25 17:30:35 +0100

    added pooling

--- app/db_raw.py
+++ app/db_raw.py
@@ -1,17 +1,26 @@
 import mysql.connector
+from mysql.connector.pooling import MySQLConnectionPool
 from flask import current_app
 from .error import APIError
 
+POOL = None
+
+def init_connection_pool(app):
+    global POOL
+    POOL = MySQLConnectionPool(
+        pool_name="humbank_pool",
+        pool_size=10,  # adjust later if needed
+        host=app.config["MYSQL_HOST"],
+        user=app.config["MYSQL_USER"],
+        password=app.config["MYSQL_PASS"],
+        database=app.config["MYSQL_DB"],
+        charset="utf8mb4",
+        autocommit=False
+    )
+
 # Create a fresh MySQL connection using credentials from config
 def getBank():
-    return mysql.connector.connect(
-        host=current_app.config["MYSQL_HOST"],
-        user=current_app.config["MYSQL_USER"],
-        password=current_app.config["MYSQL_PASS"],
-        database=current_app.config["MYSQL_DB"],
-        charset="utf8mb4",
-        autocommit=False  # IMPORTANT: we control commits manually
-    )
+    return POOL.get_connection()
 
 def id_exists(id):
     conn = getBank()
