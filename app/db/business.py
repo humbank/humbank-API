4c9383e382dcf668bfeed15d6ff16c674cae5727
Humbank <humbank.ulm@gmail.com>
2026-03-01 15:03:58 +0100

    fixed return

--- app/db/business.py
+++ app/db/business.py
@@ -131,7 +131,7 @@ def create_business(owner_username, start_balance, business_name, pin, descripti
 def can_create_business(username, cursor, limit=1):
     sql = "select count(*) from business_accounts where owner_username = %s and deleted_at = null;"
     cursor.execute(sql, (username,))
-    active_count = cursor.fetchone()
+    active_count = cursor.fetchone()[0]
     return active_count < limit
 
 
